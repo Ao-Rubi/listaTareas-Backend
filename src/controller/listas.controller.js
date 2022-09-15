8e308b039d339f83f3feffdf7bee145c1ae3a215
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-09-14 23:11:06 -0300

    validacion para el campo

--- src/controller/listas.controller.js
+++ src/controller/listas.controller.js
@@ -1,10 +1,22 @@
+import { validationResult } from "express-validator"
 import Tarea from "../models/modeloTareas"
+
 
 export const crearElemento = async (req, res) =>{
 
     try {
         console.log(req.body)
         // validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(
+                {
+                    // errors: errors.array(), //Devuelve lista de errores
+                    errors: errors.mapped(), //Devuelve el error que ocurrio
+                }
+            )
+        }
+
         // Creo objt para base de dato
 
         const tareaNueva = new Tarea({
--- src/routes/listas.routes.js
+++ src/routes/listas.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { borrarElemento, crearElemento, editarElemento, listarElementos, obtenerElemento } from "../controller/listas.controller";
-
+import { check } from "express-validator";
 const router = Router();
 
 // Crear ruta de las listas de tareas
 // dominio + /apilistas + /listas
-router.route("/elementos").get(listarElementos).post(crearElemento)
+router.route("/elementos")
+.get(listarElementos)
+.post([check("tarea").notEmpty().withMessage("El campo es obligatorio")],crearElemento)
 
-router.route("/elementos/:id").get(obtenerElemento).put(editarElemento).delete(borrarElemento)
+router.route("/elementos/:id")
+.get(obtenerElemento)
+.put(editarElemento)
+.delete(borrarElemento)
 
 export default router;
